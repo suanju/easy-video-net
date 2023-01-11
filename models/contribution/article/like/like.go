cdf856c58acf5f73c8178273a1cff022c094e171
橡皮擦 <2506152074@qq.com>
2023-01-11 18:03:26 +0800

    视频点赞模型

--- models/contribution/article/like/like.go
+++ models/contribution/article/like/like.go
@@ -5,9 +5,9 @@ import "Go-Live/models/common"
 type Likes struct {
 	common.PublicModel
 	Uid            uint `json:"uid" gorm:"uid"`
-	ContributionID uint `json:"contribution_Id"`
+	ContributionID uint `json:"contribution_id"  gorm:"contribution_id"`
 }
 
 func (Likes) TableName() string {
-	return "live_article_contribution_like"
+	return "lv_article_contribution_like"
 }
