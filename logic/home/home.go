8187d35a3c709c914c6b66ce0a4889efe5b51f23
橡皮擦 <2506152074@qq.com>
2023-01-12 22:09:52 +0800

    主页接口修改

--- logic/home/home.go
+++ logic/home/home.go
@@ -1,6 +1,7 @@
 package home
 
 import (
+	"Go-Live/models/contribution/video"
 	"Go-Live/models/home"
 	"Go-Live/models/home/rotograph"
 )
@@ -13,6 +14,12 @@ func GetHomeInfo(data *home.GetHomeInfoReceiveStruct) (results interface{}, err 
 		return nil, err
 	}
 
+	//获取主页推荐视频
+	videoList := new(video.VideosContribution)
+	err = videoList.GetHoneVideoList()
+	if err != nil {
+		return nil, err
+	}
 	response := &home.GetHomeInfoResponse{}
 	response.Response(rotographList)
 	return response, nil
