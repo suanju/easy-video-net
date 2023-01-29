f70eabff53b8d9cd444eccccb9a98e6906ffbd35
橡皮擦 <2506152074@qq.com>
2023-01-29 14:38:14 +0800

    更改位置

--- models/users/liveInfo/receive.go
+++
@@ -1,8 +0,0 @@
-package liveInfo
-
-//SaveLiveDataStruct 设置直播信息
-type SaveLiveDataStruct struct {
-	Tp     string `json:"type" binding:"required"`
-	ImgUrl string `json:"imgUrl" binding:"required"`
-	Title  string `json:"title" binding:"required"`
-}
