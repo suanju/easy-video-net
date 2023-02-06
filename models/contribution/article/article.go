09745e5234f337cb31654f793a1f3dea9441a4af
橡皮擦 <2506152074@qq.com>
2023-02-06 14:01:06 +0800

    模型修改

--- models/contribution/article/article.go
+++ models/contribution/article/article.go
@@ -3,6 +3,7 @@ package article
 import (
 	"Go-Live/global"
 	"Go-Live/models/common"
+	"Go-Live/models/contribution/article/classification"
 	"Go-Live/models/contribution/article/comments"
 	"Go-Live/models/contribution/article/like"
 	"gorm.io/datatypes"
@@ -13,6 +14,7 @@ import (
 type ArticlesContribution struct {
 	common.PublicModel
 	Uid                uint           `json:"uid" gorm:"uid"`
+	ClassificationID   uint           `json:"classification_id"  gorm:"classification_id"`
 	Title              string         `json:"title" gorm:"title"`
 	Cover              datatypes.JSON `json:"cover" gorm:"cover"`
 	Timing             int8           `json:"timing" gorm:"timing"`
@@ -24,8 +26,9 @@ type ArticlesContribution struct {
 	Heat               int            `json:"heat" gorm:"heat"`
 
 	//光联表
-	Likes    []like.Likes       `json:"likes" gorm:"foreignKey:ContributionID" `
-	Comments []comments.Comment `json:"comments" gorm:"foreignKey:ContributionID"`
+	Likes          []like.Likes                  `json:"likes" gorm:"foreignKey:ContributionID" `
+	Comments       []comments.Comment            `json:"comments" gorm:"foreignKey:ContributionID"`
+	Classification classification.Classification `json:"classification"  gorm:"foreignKey:ClassificationID"`
 }
 
 type ArticlesContributionList []ArticlesContribution
@@ -45,7 +48,16 @@ func (vc *ArticlesContribution) Create() bool {
 
 //GetListByUid 查询单个用户
 func (l *ArticlesContributionList) GetListByUid(userID uint) bool {
-	err := global.Db.Where("uid", userID).Preload("Likes").Preload("Comments").Order("created_at desc").Find(l).Error
+	err := global.Db.Where("uid", userID).Preload("Likes").Preload("Classification").Preload("Comments").Order("created_at desc").Find(l).Error
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+//GetAllCount 查询所有文章数量
+func (l *ArticlesContributionList) GetAllCount(cu *int64) bool {
+	err := global.Db.Find(l).Count(cu).Error
 	if err != nil {
 		return false
 	}
@@ -54,7 +66,7 @@ func (l *ArticlesContributionList) GetListByUid(userID uint) bool {
 
 //GetInfoByID 查询单个文章
 func (vc *ArticlesContribution) GetInfoByID(ID uint) bool {
-	err := global.Db.Where("id", ID).Preload("Likes").Preload("Comments", func(db *gorm.DB) *gorm.DB {
+	err := global.Db.Where("id", ID).Preload("Likes").Preload("Classification").Preload("Comments", func(db *gorm.DB) *gorm.DB {
 		return db.Preload("UserInfo").Order("created_at desc")
 	}).Find(vc).Error
 	if err != nil {
@@ -65,7 +77,7 @@ func (vc *ArticlesContribution) GetInfoByID(ID uint) bool {
 
 //GetArticleComments 获取评论
 func (vc *ArticlesContribution) GetArticleComments(ID uint, info common.PageInfo) bool {
-	err := global.Db.Where("id", ID).Preload("Likes").Preload("Comments", func(db *gorm.DB) *gorm.DB {
+	err := global.Db.Where("id", ID).Preload("Likes").Preload("Classification").Preload("Comments", func(db *gorm.DB) *gorm.DB {
 		return db.Preload("UserInfo").Order("created_at desc").Limit(info.Size).Offset((info.Page - 1) * info.Size)
 	}).Find(vc).Error
 	if err != nil {
