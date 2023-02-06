1f57deb578290d0fc700cbd9ae5a5432f6e3225e
橡皮擦 <2506152074@qq.com>
2023-02-06 14:08:20 +0800

    添加视频相关常量

---
+++ consts/videoSocketType.go
@@ -0,0 +1,14 @@
+package consts
+
+const (
+	/*
+		videoSocketTypeError  错误
+	*/
+	VideoSocketTypeError = "error"
+	//VideoSocketTypeNumberOfViewers 返回在线观看人数
+	VideoSocketTypeNumberOfViewers = "numberOfViewers"
+	//VideoSocketTypeSendBarrage 发送弹幕
+	VideoSocketTypeSendBarrage = "sendBarrage"
+	//VideoSocketTypeResponseBarrageNum 发送弹幕
+	VideoSocketTypeResponseBarrageNum = "responseBarrageNum"
+)
