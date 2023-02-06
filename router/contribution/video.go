476661742791ab16a212f05d0d370e87f2f75d62
橡皮擦 <2506152074@qq.com>
2023-02-06 14:08:05 +0800

    添加路由

--- router/contribution/video.go
+++ router/contribution/video.go
@@ -10,10 +10,26 @@ type VideoRouter struct {
 }
 
 func (v *VideoRouter) InitVideoRouter(Router *gin.RouterGroup) {
+	contributionControllers := new(contribution.Controllers)
+	//不需要登入
+	contributionRouterNoVerification := Router.Group("contribution").Use()
+	{
+
+		contributionRouterNoVerification.POST("/getVideoContributionByID", contributionControllers.GetVideoContributionByID)
+		contributionRouterNoVerification.GET("/video/barrage/v3/", contributionControllers.GetVideoBarrage)
+		contributionRouterNoVerification.GET("/getVideoBarrageList", contributionControllers.GetVideoBarrageList)
+		contributionRouterNoVerification.POST("/getVideoComment", contributionControllers.GetVideoComment)
+	}
+	//需要登入 参数携带
+	contributionRouterParameter := Router.Group("contribution").Use(middlewares.VerificationTokenAsParameter())
+	{
+		contributionRouterParameter.POST("/video/barrage/v3/", contributionControllers.SendVideoBarrage)
+	}
+	//请求头携带
 	contributionRouter := Router.Group("contribution").Use(middlewares.VerificationToken())
 	{
-		contributionControllers := new(contribution.Controllers)
 		contributionRouter.POST("/createVideoContribution", contributionControllers.CreateVideoContribution)
-		contributionRouter.POST("/getVideoContributionByID", contributionControllers.GetVideoContributionByID)
+		contributionRouter.POST("/videoPostComment", contributionControllers.VideoPostComment)
 	}
+
 }
