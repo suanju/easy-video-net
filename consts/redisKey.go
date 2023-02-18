8c39ec0387bad1c6d76fbc01f1dc5db869e3e84c
橡皮擦 <2506152074@qq.com>
2023-02-18 17:55:29 +0800

    redisKey添加

--- consts/redisKey.go
+++ consts/redisKey.go
@@ -5,12 +5,18 @@ const (
 		RegEmailVerCode	注册验证码
 		RegEmailVerCodeByForget 找回密码验证码
 	*/
-	RegEmailVerCode         = "regEmailVerCode"
-	RegEmailVerCodeByForget = "regEmailVerCodeByForget"
+	RegEmailVerCode                       = "regEmailVerCode"
+	RegEmailVerCodeByForget               = "regEmailVerCodeByForget"
+	EmailVerificationCodeByChangePassword = "emailVerificationCodeByChangePassword"
 
 	/*
 		RegEmailVerCode	注册验证码
 		RegEmailVerCodeByForget 找回密码验证码
 	*/
 	LiveRoomHistoricalBarrage = "liveRoomHistoricalBarrage_"
+
+	//LiveRoom 直播房间
+	LiveRoom = "liveRoom_"
+	//VideoWatchByID 观看视频
+	VideoWatchByID = "videoWatchBy_"
 )
