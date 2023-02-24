fb528f3b3deff76136e087118db7a8c20c88a900
橡皮擦 <2506152074@qq.com>
2023-02-24 22:50:47 +0800

    修改模型方法

--- models/users/attention/attention.go
+++ models/users/attention/attention.go
@@ -3,21 +3,27 @@ package attention
 import (
 	"Go-Live/global"
 	"Go-Live/models/common"
+	"Go-Live/models/users"
 )
 
 type Attention struct {
 	common.PublicModel
 	Uid         uint `json:"uid" gorm:"uid"`
 	AttentionID uint `json:"attention_id" gorm:"attention_id"`
+
+	UserInfo          users.User `json:"user_info" gorm:"foreignKey:Uid"`
+	AttentionUserInfo users.User `json:"attention_user_info" gorm:"foreignKey:AttentionID"`
 }
+
+type AttentionsList []Attention
 
 func (Attention) TableName() string {
 	return "lv_users_attention"
 }
 
 //Create 添加数据
-func (li *Attention) Create() bool {
-	err := global.Db.Create(&li).Error
+func (at *Attention) Create() bool {
+	err := global.Db.Create(&at).Error
 	if err != nil {
 		return false
 	}
@@ -25,8 +31,8 @@ func (li *Attention) Create() bool {
 }
 
 //Delete 删除数据
-func (li *Attention) Delete() bool {
-	err := global.Db.Where("uid", li.Uid).Updates(&li).Error
+func (at *Attention) Delete() bool {
+	err := global.Db.Where("uid", at.Uid).Updates(&at).Error
 	if err != nil {
 		return false
 	}
@@ -34,11 +40,11 @@ func (li *Attention) Delete() bool {
 }
 
 //Attention 关注用户
-func (li *Attention) Attention(uid uint, aid uint) bool {
-	err := global.Db.Where(Attention{Uid: uid, AttentionID: aid}).Find(&li).Error
-	if li.ID > 0 {
+func (at *Attention) Attention(uid uint, aid uint) bool {
+	err := global.Db.Where(Attention{Uid: uid, AttentionID: aid}).Find(&at).Error
+	if at.ID > 0 {
 		//已关注
-		err = global.Db.Where("id", li.ID).Delete(&li).Error
+		err = global.Db.Where("id", at.ID).Delete(&at).Error
 	} else {
 		//未关注
 		err = global.Db.Create(&Attention{Uid: uid, AttentionID: aid}).Error
@@ -51,9 +57,9 @@ func (li *Attention) Attention(uid uint, aid uint) bool {
 }
 
 //IsAttention  判断是否关注用户
-func (li *Attention) IsAttention(uid uint, aid uint) bool {
-	_ = global.Db.Where(Attention{Uid: uid, AttentionID: aid}).Find(&li).Error
-	if li.ID > 0 {
+func (at *Attention) IsAttention(uid uint, aid uint) bool {
+	_ = global.Db.Where(Attention{Uid: uid, AttentionID: aid}).Find(&at).Error
+	if at.ID > 0 {
 		return true
 	} else {
 		return false
@@ -61,9 +67,9 @@ func (li *Attention) IsAttention(uid uint, aid uint) bool {
 }
 
 //GetAttentionNum 获取关注数量
-func (li *Attention) GetAttentionNum(uid uint) (*int64, error) {
+func (at *Attention) GetAttentionNum(uid uint) (*int64, error) {
 	num := new(int64)
-	err := global.Db.Model(li).Where(Attention{Uid: uid}).Count(num).Error
+	err := global.Db.Model(at).Where(Attention{Uid: uid}).Count(num).Error
 	if err != nil {
 		return num, err
 	}
@@ -71,11 +77,43 @@ func (li *Attention) GetAttentionNum(uid uint) (*int64, error) {
 }
 
 //GetVermicelliNum 获取粉丝数量
-func (li *Attention) GetVermicelliNum(uid uint) (*int64, error) {
+func (at *Attention) GetVermicelliNum(uid uint) (*int64, error) {
 	num := new(int64)
-	err := global.Db.Model(li).Where(Attention{AttentionID: uid}).Count(num).Error
+	err := global.Db.Model(at).Where(Attention{AttentionID: uid}).Count(num).Error
 	if err != nil {
 		return num, err
 	}
 	return num, nil
+}
+
+//GetAttentionList 获取关注列表
+func (al *AttentionsList) GetAttentionList(uid uint) error {
+	err := global.Db.Where("uid", uid).Preload("AttentionUserInfo").Find(al).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//GetVermicelliList 获取粉丝列表
+func (al *AttentionsList) GetVermicelliList(uid uint) error {
+	err := global.Db.Where("attention_id", uid).Preload("UserInfo").Find(al).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//GetAttentionListByIdArr 获取关注列表 id数组
+func (al *AttentionsList) GetAttentionListByIdArr(uid uint) (arr []uint, err error) {
+	arr = make([]uint, 0)
+	err = global.Db.Where("uid", uid).Find(al).Error
+	if err != nil {
+		return arr, err
+	}
+	//自需要数组
+	for _, v := range *al {
+		arr = append(arr, v.AttentionID)
+	}
+	return arr, nil
 }
