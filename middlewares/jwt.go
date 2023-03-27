b7b5378b504e3b3108531d20b088129c0108f71b
橡皮擦 <2506152074@qq.com>
2023-03-27 10:03:20 +0800

    中间件修改

--- middlewares/jwt.go
+++ middlewares/jwt.go
@@ -4,7 +4,10 @@ import (
 	"Go-Live/models/users"
 	"Go-Live/utils/jwt"
 	ControllersCommon "Go-Live/utils/response"
+	Response "Go-Live/utils/response"
 	"Go-Live/utils/validator"
+	"github.com/gorilla/websocket"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
@@ -33,7 +36,7 @@ func VerificationToken() gin.HandlerFunc {
 	}
 }
 
-//VerificationTokenAsParameter 请求参数中携带token
+//VerificationTokenAsParameter body参数中携带token
 func VerificationTokenAsParameter() gin.HandlerFunc {
 	type qu struct {
 		Token string `json:"token"`
@@ -88,5 +91,37 @@ func VerificationTokenNotNecessary() gin.HandlerFunc {
 			c.Set("currentUserName", u.Username)
 			c.Next()
 		}
+	}
+}
+
+func VerificationTokenAsSocket() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		//升级ws 以便返回消息
+		conn, err := (&websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}).Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			http.NotFound(c.Writer, c.Request)
+			c.Abort()
+			return
+		}
+		token := c.Query("token")
+		claim, err := jwt.ParseToken(token)
+		if err != nil {
+			Response.NotLoginWs(conn, "Token 验证失败")
+			_ = conn.Close()
+			c.Abort()
+			return
+		}
+		u := new(users.User)
+		if !u.IsExistByField("id", claim.UserID) {
+			//没有改用户的情况下
+			Response.NotLoginWs(conn, "用户异常")
+			_ = conn.Close()
+			c.Abort()
+			return
+		}
+		c.Set("uid", u.ID)
+		c.Set("conn", conn)
+		c.Set("currentUserName", u.Username)
+		c.Next()
 	}
 }
