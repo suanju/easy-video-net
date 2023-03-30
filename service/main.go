a57fe2aa7b66b9cd8b24ae49a4a724e0a621bf4d
橡皮擦 <2506152074@qq.com>
2023-03-30 11:17:22 +0800

    修改提示方式

--- service/main.go
+++ service/main.go
@@ -3,24 +3,14 @@ package main
 import (
 	_ "easy-video-net/global/database/mysql"
 	_ "easy-video-net/global/database/redis"
-	"easy-video-net/logic/contribution/videoSocket"
-	liveSocket "easy-video-net/logic/live/socket"
-	"easy-video-net/logic/users/chatByUserSocket"
-	"easy-video-net/logic/users/chatSocket"
-	"easy-video-net/logic/users/noticeSocket"
 	"easy-video-net/router"
+	_ "easy-video-net/utils/socket"
 	"easy-video-net/utils/testing"
 )
 
 func main() {
 	//检查直播服务
 	testing.LiveSeverTesting()
-	//开启直播和视频socket
-	go liveSocket.Severe.Start()
-	go videoSocket.Severe.Start()
-	go noticeSocket.Severe.Start()
-	go chatSocket.Severe.Start()
-	go chatByUserSocket.Severe.Start()
 	//ces
 	router.InitRouter()
 
