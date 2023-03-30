b7b5f8a67b6f6f8e26d0ed507523f3f6ac56f670
橡皮擦 <2506152074@qq.com>
2023-03-30 19:55:56 +0800

    路由变更

--- service/router/ws/socket.go
+++ service/router/ws/socket.go
@@ -17,10 +17,10 @@ func (r *Router) InitSocketRouter(Router *gin.RouterGroup) {
 		userControllers := new(users.UserControllers)
 		liveControllers := new(live.LivesControllers)
 		contributionControllers := new(contribution.Controllers)
-		socketRouter.GET("/notice", userControllers.NoticeSocket)
-		socketRouter.GET("/chat", userControllers.ChatSocket)
-		socketRouter.GET("/chatUser", userControllers.ChatByUserSocket)
+		socketRouter.GET("/noticeSocket", userControllers.NoticeSocket)
+		socketRouter.GET("/chatSocket", userControllers.ChatSocket)
+		socketRouter.GET("/chatUserSocket", userControllers.ChatByUserSocket)
 		socketRouter.GET("/liveSocket", liveControllers.LiveSocket)
-		socketRouter.GET("/sokcet", contributionControllers.VideoSocket)
+		socketRouter.GET("/videoSocket", contributionControllers.VideoSocket)
 	}
 }
