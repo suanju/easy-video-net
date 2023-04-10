6776686cb8a62c7a3085bf15d7b6be4780eb431a
橡皮擦 <2506152074@qq.com>
2023-04-10 20:00:38 +0800

    视频发布页

--- web/src/views/creation/contribute/contributePage/vdeoContribution.vue
+++ web/src/views/creation/contribute/contributePage/vdeoContribution.vue
@@ -2,10 +2,10 @@
     <!-- 上传 -->
     <div class="vdeo-contribution ">
         <div class="upload-box animate__animated animate__bounceIn" v-show="!form.isShow">
-            <el-upload class="upload" drag action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
-                multiple :on-success="handle.handleFileSuccess" :on-error="handle.handleFileError"
-                :show-file-list="false" :before-upload="handle.beforeFileUpload" :auto-upload="true"
-                :http-request="handle.RedefineUploadFile" accept=".mp4,.avi,.mov">
+            <el-upload class="upload" drag action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15" multiple
+                :on-success="handle.handleFileSuccess" :on-error="handle.handleFileError" :show-file-list="false"
+                :before-upload="handle.beforeFileUpload" :auto-upload="true" :http-request="handle.RedefineUploadFile"
+                accept=".mp4,.avi,.mov">
                 <el-icon class="el-icon--upload">
                     <upload-filled />
                 </el-icon>
@@ -25,10 +25,10 @@
             </el-upload>
         </div>
         <!-- 配置上传信息 -->
-        <div class="form-box animate__animated animate__bounceIn" v-show="form.isShow ">
+        <div class="form-box animate__animated animate__bounceIn" v-show="form.isShow">
             <!-- 视频预览 -->
             <div class="video-preview">
-            <video class="video" ref="video" src=""  ></video>
+                <video class="video" ref="video" src=""></video>
             </div>
             <p>文件上传进度</p>
             <el-progress :text-inside="true" :stroke-width="16" :percentage="uploadFileformation.progress" />
@@ -55,17 +55,17 @@
                         <el-radio :label="true">转载</el-radio>
                     </el-radio-group>
                 </el-form-item>
-                <el-form-item label="定时发布" v-show="props.type != 'edit'">
+                <!-- <el-form-item label="定时发布" v-show="props.type != 'edit'">
                     <el-switch v-model="form.timing" />
                 </el-form-item>
                 <el-form-item label="选择时间" v-show="form.timing" class="animate__animated animate__fadeIn">
                     <el-col :span="7">
                         <el-date-picker v-model="form.date1time" type="datetime" placeholder="请选择定时发布时间" />
                     </el-col>
-                </el-form-item>
+                </el-form-item> -->
                 <el-form-item label="标签" class="label-box">
-                    <el-tag v-for="tag in form.label" :key="tag" closable :disable-transitions="false"
-                        class="label-item" @close="labelHandl.handleClose(tag)">
+                    <el-tag v-for="tag in form.label" :key="tag" closable :disable-transitions="false" class="label-item"
+                        @close="labelHandl.handleClose(tag)">
                         {{ tag }}
                     </el-tag>
                     <el-input v-if="form.labelInputVisible" ref="labelInputRef" v-model="form.labelText" size="small"
@@ -81,7 +81,8 @@
                 </el-form-item>
                 <el-form-item>
                     <el-button type="primary"
-                        @click="useSaveData(form, uploadFileformation, uploadCoveration, ruleFormRef, router ,props)">{{props.type == "edit" ? "更新视频" : "发布视频"}}</el-button>
+                        @click="useSaveData(form, uploadFileformation, uploadCoveration, ruleFormRef, router, props)">{{ props.type
+                            == "edit" ? "更新视频" : "发布视频" }}</el-button>
                 </el-form-item>
             </el-form>
         </div>
@@ -89,27 +90,26 @@
 </template>
 
 <script setup lang="ts">
-import { useVdeoContributionProp, useHandleFileMethod, useInit, useHandleCoverMethod, userLabelHandlMethod, useSaveData, useRules } from "@/logic/creation/contribute/contributePage/vdeoContribution"
-import { UploadFilled, Upload } from '@element-plus/icons-vue'
-import {  onMounted } from 'vue';
-import { Plus } from '@element-plus/icons-vue'
-import { ElInput} from 'element-plus'
+import { useHandleCoverMethod, useHandleFileMethod, useInit, useRules, useSaveData, useVdeoContributionProp, userLabelHandlMethod } from "@/logic/creation/contribute/contributePage/vdeoContribution";
+import { Plus, Upload, UploadFilled } from '@element-plus/icons-vue';
+import { ElInput } from 'element-plus';
+import { onMounted } from 'vue';
 
 const props = defineProps({
-    type : {
-        type : String
+    type: {
+        type: String
     }
 })
 
-const { form, uploadFileformation, uploadCoveration, labelInputRef, router, ruleFormRef ,video ,editVideoStore } = useVdeoContributionProp()
-const handle = useHandleFileMethod(uploadFileformation, form ,video)
+const { form, uploadFileformation, uploadCoveration, labelInputRef, router, ruleFormRef, video, editVideoStore } = useVdeoContributionProp()
+const handle = useHandleFileMethod(uploadFileformation, form, video)
 const handleCover = useHandleCoverMethod(uploadCoveration, form)
 const labelHandl = userLabelHandlMethod(form, labelInputRef)
 const { videoContributionRules } = useRules()
 
 
 onMounted(() => {
-    useInit(uploadFileformation, uploadCoveration ,form, props,editVideoStore)
+    useInit(uploadFileformation, uploadCoveration, form, props, editVideoStore)
 })
 </script>
 
