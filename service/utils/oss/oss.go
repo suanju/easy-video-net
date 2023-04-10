249717f2fdcd644c0182d2c7b3dfdfe33be3b8e5
橡皮擦 <2506152074@qq.com>
2023-04-10 19:56:57 +0800

    阿里云工具类

--- service/utils/oss/oss.go
+++ service/utils/oss/oss.go
@@ -1,33 +1,18 @@
 package oss
 
 import (
-	"crypto/hmac"
-	"crypto/sha1"
 	"easy-video-net/global"
-	"easy-video-net/models/config/upload"
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
+	ice20201109 "github.com/alibabacloud-go/ice-20201109/v2/client"
 	sts20150401 "github.com/alibabacloud-go/sts-20150401/v2/client"
 	util "github.com/alibabacloud-go/tea-utils/v2/service"
 	"github.com/alibabacloud-go/tea/tea"
-	"hash"
-	"io"
-	"time"
 )
 
-// 请填写您的AccessKeyId。
 var accessKeyId = global.Config.AliyunOss.AccessKeyId
 
-// 请填写您的AccessKeySecret。
 var accessKeySecret = global.Config.AliyunOss.AccessKeySecret
-
-// host的格式为 bucket-name.endpoint ，请替换为您的真实信息。
-var host = global.Config.AliyunOss.Host
-
-// callbackUrl为 上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
-var callbackUrl = global.Config.AliyunOss.CallbackUrl
 
 var roleArn = global.Config.AliyunOss.RoleArn
 
@@ -36,10 +21,6 @@ var roleSessionName = global.Config.AliyunOss.RoleSessionName
 var durationSeconds = global.Config.AliyunOss.DurationSeconds
 
 var endpoint = global.Config.AliyunOss.Endpoint
-
-// 用户上传文件时指定的前缀。
-//var uploadDir string = "upload/img/user/liveCover/"
-var expireTime int64 = 30
 
 type ConfigStruct struct {
 	Expiration string     `json:"expiration"`
@@ -61,73 +42,9 @@ type PolicyToken struct {
 	Callback    string `json:"callback"`
 }
 
-func GetPolicyToken(_interface string) (results interface{}, err error) {
-	//获取当前接口对于的储存路径
-	method := new(upload.Upload)
-	if !method.IsExistByField("interface", _interface) {
-		return nil, fmt.Errorf("上传接口不存在")
-	}
-	if len(method.Path) == 0 {
-		return nil, fmt.Errorf("请联系管理员设置接口保存路径")
-	}
-	uploadDir := method.Path
-	now := time.Now().Unix()
-	expireEnd := now + expireTime
-	var tokenExpire = getGmtIso8601(expireEnd)
-
-	//create post policy json
-	var config ConfigStruct
-	config.Expiration = tokenExpire
-	var condition []string
-	condition = append(condition, "starts-with")
-	condition = append(condition, "$key")
-	condition = append(condition, uploadDir)
-	config.Conditions = append(config.Conditions, condition)
-
-	//calculate signature
-	result, err := json.Marshal(config)
-	debate := base64.StdEncoding.EncodeToString(result)
-	h := hmac.New(func() hash.Hash { return sha1.New() }, []byte(accessKeySecret))
-	_, err = io.WriteString(h, debate)
-	if err != nil {
-		return PolicyToken{}, nil
-	}
-	signedStr := base64.StdEncoding.EncodeToString(h.Sum(nil))
-
-	var callbackParam CallbackParam
-	callbackParam.CallbackUrl = callbackUrl
-	callbackParam.CallbackBody = "filename=${object}&size=${size}&mimeType=${mimeType}&height=${imageInfo.height}&width=${imageInfo.width}"
-	callbackParam.CallbackBodyType = "application/x-www-form-urlencoded"
-	callbackStr, err := json.Marshal(callbackParam)
-	if err != nil {
-		fmt.Println("callback json err:", err)
-	}
-	callbackBase64 := base64.StdEncoding.EncodeToString(callbackStr)
-
-	var policyToken PolicyToken
-	policyToken.AccessKeyId = accessKeyId
-	policyToken.Host = host
-	policyToken.Expire = expireEnd
-	policyToken.Signature = signedStr
-	policyToken.Directory = uploadDir
-	policyToken.Policy = debate
-	policyToken.Callback = callbackBase64
-
-	return policyToken, nil
-}
-
-// CreateClient
-// * 使用AK&SK初始化账号Client
-// * @param accessKeyId
-// * @param accessKeySecret
-// * @return Client
-// * @throws Exception
-///**
-func CreateClient(accessKeyId *string, accessKeySecret *string) (_result *sts20150401.Client, _err error) {
+func CreateStsClient(accessKeyId *string, accessKeySecret *string) (_result *sts20150401.Client, _err error) {
 	config := &openapi.Config{
-		// 必填，您的 AccessKey ID
-		AccessKeyId: accessKeyId,
-		// 必填，您的 AccessKey Secret
+		AccessKeyId:     accessKeyId,
 		AccessKeySecret: accessKeySecret,
 	}
 	// 访问的域名
@@ -138,7 +55,7 @@ func CreateClient(accessKeyId *string, accessKeySecret *string) (_result *sts201
 }
 
 func GteStsInfo() (*sts20150401.AssumeRoleResponseBodyCredentials, error) {
-	client, err := CreateClient(tea.String(accessKeyId), tea.String(accessKeySecret))
+	client, err := CreateStsClient(tea.String(accessKeyId), tea.String(accessKeySecret))
 	if err != nil {
 		return nil, err
 	}
@@ -152,7 +69,6 @@ func GteStsInfo() (*sts20150401.AssumeRoleResponseBodyCredentials, error) {
 		if r := tea.Recover(recover()); r != nil {
 		}
 	}()
-	// 复制代码运行请自行打印 API 的返回值
 	res, err := client.AssumeRoleWithOptions(assumeRoleRequest, runtime)
 	if err != nil {
 		return nil, err
@@ -163,7 +79,115 @@ func GteStsInfo() (*sts20150401.AssumeRoleResponseBodyCredentials, error) {
 	return res.Body.Credentials, nil
 }
 
-func getGmtIso8601(expireEnd int64) string {
-	var tokenExpire = time.Unix(expireEnd, 0).UTC().Format("2006-01-02T15:04:05Z")
-	return tokenExpire
+func CreateIceClient(accessKeyId *string, accessKeySecret *string) (_result *ice20201109.Client, _err error) {
+	config := &openapi.Config{
+		AccessKeyId:     accessKeyId,
+		AccessKeySecret: accessKeySecret,
+	}
+	// 访问的域名
+	config.Endpoint = tea.String("ice.cn-hangzhou.aliyuncs.com")
+	result := &ice20201109.Client{}
+	result, err := ice20201109.NewClient(config)
+	return result, err
+}
+func RegisterMediaInfo(inputUrl, mediaType, Title string) (body *ice20201109.RegisterMediaInfoResponseBody, err error) {
+	client, err := CreateIceClient(tea.String(accessKeyId), tea.String(accessKeySecret))
+	if err != nil {
+		global.Logger.Errorf("初始化cilent失败 err : %s", err.Error())
+	}
+	registerMediaInfoRequest := &ice20201109.RegisterMediaInfoRequest{
+		Overwrite: tea.Bool(true),
+		InputURL:  tea.String(inputUrl),
+		MediaType: tea.String(mediaType),
+		Title:     tea.String(Title),
+	}
+	runtime := &util.RuntimeOptions{}
+	defer func() {
+		if r := tea.Recover(recover()); r != nil {
+		}
+	}()
+	result, _err := client.RegisterMediaInfoWithOptions(registerMediaInfoRequest, runtime)
+	if _err != nil {
+		global.Logger.Errorf("注册媒资失败 err %s ", err.Error())
+		fmt.Println(_err)
+	}
+	if *result.StatusCode != 200 {
+		return nil, err
+	}
+	return result.Body, nil
+}
+
+func GetMediaInfo(mediaID *string) (body *ice20201109.GetMediaInfoResponse, err error) {
+	client, err := CreateIceClient(tea.String(accessKeyId), tea.String(accessKeySecret))
+	if err != nil {
+		global.Logger.Errorf("初始化cilent失败 err : %s", err.Error())
+	}
+
+	getMediaInfoRequest := &ice20201109.GetMediaInfoRequest{
+		MediaId: mediaID,
+	}
+	runtime := &util.RuntimeOptions{}
+	if r := tea.Recover(recover()); r != nil {
+	}
+	result, err := client.GetMediaInfoWithOptions(getMediaInfoRequest, runtime)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func SubmitTranscodeJob(taskName, mediaID, outputUrl, template string) (body *ice20201109.SubmitTranscodeJobResponseBody, err error) {
+	client, err := CreateIceClient(tea.String(accessKeyId), tea.String(accessKeySecret))
+	if err != nil {
+		global.Logger.Errorf("初始化cilent失败 err : %s", err.Error())
+	}
+	inputGroup0 := &ice20201109.SubmitTranscodeJobRequestInputGroup{
+		Type:  tea.String("Media"),
+		Media: tea.String(mediaID),
+	}
+	outputGroup0Output := &ice20201109.SubmitTranscodeJobRequestOutputGroupOutput{
+		Type:  tea.String("OSS"),
+		Media: tea.String(outputUrl),
+	}
+	outputGroup0ProcessConfigTranscode := &ice20201109.SubmitTranscodeJobRequestOutputGroupProcessConfigTranscode{
+		TemplateId: tea.String(template),
+	}
+	outputGroup0ProcessConfig := &ice20201109.SubmitTranscodeJobRequestOutputGroupProcessConfig{
+		Transcode: outputGroup0ProcessConfigTranscode,
+	}
+	outputGroup0 := &ice20201109.SubmitTranscodeJobRequestOutputGroup{
+		ProcessConfig: outputGroup0ProcessConfig,
+		Output:        outputGroup0Output,
+	}
+	submitTranscodeJobRequest := &ice20201109.SubmitTranscodeJobRequest{
+		OutputGroup: []*ice20201109.SubmitTranscodeJobRequestOutputGroup{outputGroup0},
+		Name:        tea.String(taskName),
+		InputGroup:  []*ice20201109.SubmitTranscodeJobRequestInputGroup{inputGroup0},
+	}
+	runtime := &util.RuntimeOptions{}
+	defer func() {
+		if r := tea.Recover(recover()); r != nil {
+		}
+	}()
+	result, err := client.SubmitTranscodeJobWithOptions(submitTranscodeJobRequest, runtime)
+	if err != nil {
+		return nil, err
+	}
+	return result.Body, nil
+}
+
+func GetTranscodeJob(taskID string) (body *ice20201109.GetTranscodeJobResponseBody, err error) {
+	client, err := CreateIceClient(tea.String(accessKeyId), tea.String(accessKeySecret))
+	if err != nil {
+		global.Logger.Errorf("初始化cilent失败 err : %s", err.Error())
+	}
+	getTranscodeJobRequest := &ice20201109.GetTranscodeJobRequest{
+		ParentJobId: tea.String("9ce776d01f034d23b31bc68ffbb2e276"),
+	}
+	runtime := &util.RuntimeOptions{}
+	result, err := client.GetTranscodeJobWithOptions(getTranscodeJobRequest, runtime)
+	if err != nil {
+		return nil, err
+	}
+	return result.Body, nil
 }
