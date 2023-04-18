83d5c7a5c385b501e3e0ac81fa3dd9b544f4cef4
橡皮擦 <2506152074@qq.com>
2023-04-18 12:31:31 +0800

    测试环境自启动

--- service/global/live/live.go
+++ service/global/live/live.go
@@ -6,35 +6,44 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"os"
 	"os/exec"
+	"runtime"
 )
 
 func init() {
 	go func() {
 		if !global.Config.ProjectConfig.ProjectStates {
 			//测试环境快速开启直播服务
-			err := Start()
-			if err != nil {
-				global.Logger.Error("开启直播服务失败")
+			if runtime.GOOS == "windows" {
+				err := Start()
+				if err != nil {
+					global.Logger.Error("开启直播服务失败")
+				}
 			}
 		}
 	}()
 }
 
 func Start() error {
-	path := location.GetCurrentAbPath()
-	path = path + `\Config\live\`
-	cmd := exec.Command("cmd.exe", "/c", "start "+path+"live-go.exe")
-	//获取输出对象，可以从该对象中读取输出结果
+	dir, err := location.GetCurrentAbPath()
+	if err != nil {
+		global.Logger.Errorf("测试环境快速开启livego服务失败,获取当前可执行文件目录失败")
+		return err
+	}
+	path := dir + `\Config\live\live-go.exe`
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		global.Logger.Errorf("测试环境快速开启livego服务失败 文件 %s 不存在", path)
+		return err
+	}
+	cmd := exec.Command("cmd.exe", "/c", "start "+path)
 	if stdio, err := cmd.StdoutPipe(); err != nil {
 		return err
-		log.Fatal(err)
 	} else {
 		// 保证关闭输出流
 		defer func(stdio io.ReadCloser) {
 			err := stdio.Close()
 			if err != nil {
-
 			}
 		}(stdio)
 		// 运行命令
