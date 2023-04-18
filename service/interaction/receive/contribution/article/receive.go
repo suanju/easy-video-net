e9c608ff5e28fbd9ca970c5817e894985f64effe
橡皮擦 <2506152074@qq.com>
2023-04-18 12:31:41 +0800

    接收结构体

--- service/interaction/receive/contribution/article/receive.go
+++ service/interaction/receive/contribution/article/receive.go
@@ -2,20 +2,17 @@ package article
 
 import (
 	"easy-video-net/models/common"
-	"time"
 )
 
 type CreateArticleContributionReceiveStruct struct {
-	Cover                         string    `json:"cover" binding:"required"`
-	CoverUploadType               string    `json:"coverUploadType" binding:"required"`
-	ArticleContributionUploadType string    `json:"articleContributionUploadType" binding:"required"`
-	Title                         string    `json:"title" binding:"required"`
-	Timing                        *bool     `json:"timing" binding:"required"`
-	TimingTime                    time.Time `json:"timingTime"`
-	Label                         []string  `json:"label" binding:"required"`
-	Content                       string    `json:"content" binding:"required"`
-	Comments                      *bool     `json:"comments"  binding:"required"`
-	ClassificationID              uint      `json:"classification_id"`
+	Cover                         string   `json:"cover" binding:"required"`
+	CoverUploadType               string   `json:"coverUploadType" binding:"required"`
+	ArticleContributionUploadType string   `json:"articleContributionUploadType" binding:"required"`
+	Title                         string   `json:"title" binding:"required"`
+	Label                         []string `json:"label" binding:"required"`
+	Content                       string   `json:"content" binding:"required"`
+	Comments                      *bool    `json:"comments"  binding:"required"`
+	ClassificationID              uint     `json:"classification_id"`
 }
 
 type UpdateArticleContributionReceiveStruct struct {
