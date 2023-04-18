e63b5e85f34cb62b9a7de56930e44c9033633480
橡皮擦 <2506152074@qq.com>
2023-04-18 12:34:36 +0800

    入口调整

--- service/main.go
+++ service/main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	_ "easy-video-net/global/database/mysql"
-	_ "easy-video-net/global/database/redis"
+	_ "easy-video-net/global/database/mysql" //初始化mysql
+	_ "easy-video-net/global/database/redis" //初始化redis
 	"easy-video-net/router"
-	_ "easy-video-net/utils/socket"
-	"easy-video-net/utils/testing"
+	_ "easy-video-net/utils/socket"  //初始化socket
+	_ "easy-video-net/utils/testing" //运行环境检查
 )
 
 func main() {
-	//检查直播服务
-	testing.LiveSeverTesting()
-	//ces
+	//注册路由
 	router.InitRouter()
-
 }
