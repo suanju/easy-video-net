2023ee60047baecded3fbf18e060bc51c52e7bd8
橡皮擦 <2506152074@qq.com>
2023-04-26 09:25:03 +0800

    添加重试机制

--- service/global/database/mysql/mysql.go
+++ service/global/database/mysql/mysql.go
@@ -1,15 +1,20 @@
 package mysql
 
 import (
+	"context"
 	"easy-video-net/global/config"
 	globalLog "easy-video-net/global/logrus"
 	"fmt"
+	"github.com/sethvargo/go-retry"
 	"github.com/sirupsen/logrus"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 	"log"
+	"time"
 )
+
+var Db *gorm.DB
 
 type MyWriter struct {
 	log *logrus.Logger
@@ -26,7 +31,6 @@ func NewMyWriter() *MyWriter {
 }
 
 func ReturnsInstance() *gorm.DB {
-	var err error
 	var mysqlConfig = config.Config.SqlConfig
 	//sql日志记录
 	myLogger := logger.New(
@@ -36,16 +40,26 @@ func ReturnsInstance() *gorm.DB {
 			LogLevel: logger.Error,
 		},
 	)
-	// 创建链接
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true&loc=Local", mysqlConfig.User, mysqlConfig.Password, mysqlConfig.IP, mysqlConfig.Port, mysqlConfig.Database)
-	Db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: myLogger,
-	})
-	if err != nil {
-		log.Fatalf("数据库链接错误- %v \n", err)
-	}
-	if Db.Error != nil {
-		log.Fatalf("数据库错误- %v \n", Db.Error)
+	b := retry.NewFibonacci(3 * time.Second)
+	//var Db *gorm.DB
+	ctx := context.Background()
+	if err := retry.Do(ctx, retry.WithMaxRetries(3, b), func(ctx context.Context) error {
+		// 创建链接
+		var err error
+		dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true&loc=Local", mysqlConfig.User, mysqlConfig.Password, mysqlConfig.IP, mysqlConfig.Port, mysqlConfig.Database)
+		Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+			Logger: myLogger,
+		})
+		if err != nil {
+			return err
+		}
+		if Db.Error != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		// handle error
+		log.Fatalf("重连3次后依旧数据库链接错误- %v \n", err)
 	}
 	return Db
 }
--- service/go.mod
+++ service/go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/gorilla/websocket v1.5.0
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
+	github.com/sethvargo/go-retry v0.2.4
 	github.com/sirupsen/logrus v1.8.1
 	google.golang.org/protobuf v1.28.0
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
